797272ff93f0d9797d6101d07c81f52e5c854bf0
Cameron Stanley <cam96stanley@gmail.com>
2025-07-15 04:19:55 -0400

    initialized and started swagger docs

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 from app.models import db
 from app.extenstions import ma
 from app.blueprints.user import user_bp
 from app.blueprints.blog import blog_bp
+
+SWAGGER_URL = "/api/docs"
+API_URL = "/static/swagger.yaml"
+
+swagger_blueprint = get_swaggerui_blueprint(
+  SWAGGER_URL, 
+  API_URL,
+  config = {
+    "app_name": "Blog API"
+  }
+)
 
 def create_app(config_name):
   app = Flask(__name__)
@@ -13,5 +25,6 @@ def create_app(config_name):
   
   app.register_blueprint(user_bp, url_prefix="/users")
   app.register_blueprint(blog_bp, url_prefix="/blogs")
+  app.register_blueprint(swagger_blueprint, url_prefix=SWAGGER_URL)
   
   return app
