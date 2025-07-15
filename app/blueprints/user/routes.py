54ab3f4332a9150df8831323af733a66c3c7e43f
Cameron Stanley <cam96stanley@gmail.com>
2025-07-14 22:09:54 -0400

    created login user route and fixed type in check password fn

--- app/blueprints/user/routes.py
+++ app/blueprints/user/routes.py
@@ -4,7 +4,41 @@ from sqlalchemy.exc import IntegrityError
 from app.models import db, User
 from app.blueprints.user.schemas import user_schema, create_user_schema
 from app.blueprints.user import user_bp
-from utils.auth import hash_password
+from utils.auth import hash_password, check_password, generate_token
+
+
+@user_bp.route("/login", methods=["POST"])
+def login():
+  try:
+    data = request.get_json()
+
+    if not data or not data.get("email") or not data.get("password"):
+      return jsonify({"error": "Email and password are required"}), 400
+    
+    user = db.session.query(User).filter_by(email=data["email"]).first()
+    
+    if not user or not check_password(data["password"], user.password):
+      return jsonify({"error": "Invalid email or password"}), 401
+    
+    token = generate_token(user.id)
+    
+    return jsonify({
+      "status": "success",
+      "message": "Login successfull",
+      "token": token,
+      "user": {
+        "id": user.id,
+        "name": user.name,
+        "email": user.email,
+      }
+    }), 200
+    
+  except Exception as e:
+    return jsonify({
+      "error": "Internal server error",
+      "details": str(e)
+    }), 500
+
 
 @user_bp.route("/", methods=["POST"])
 def create_user():
--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class DevelopmentConfig:
   SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+  SECRET_KEY = os.environ.get("SECRET_KEY") or "super secret key"
   CACHE_TYPE = "SimpleCache"
   DEBUG = True
